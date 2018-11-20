6624ffbdc522126a1f7d44026c1ff3cd8e3cf3b8
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2018-11-20 23:24:49 +0300

    PointProgram

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
@@ -4,7 +4,7 @@ public class PointProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(2.0, 0.0);
+    Point p1 = new Point(3.0, 0.0);
     Point p2 = new Point(5.0, 0.0);
 
     System.out.println("Расстояние между р1 и р2 " + Point.distance(p1, p2));
