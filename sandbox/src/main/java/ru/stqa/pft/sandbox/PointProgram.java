daaaf594a19e37283d8963a2b27672626ff6a1ee
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2018-11-20 18:32:22 +0300

    Point first case

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class PointProgram {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(2.0, 0.0);
+    Point p2 = new Point(5.0, 0.0);
+
+    System.out.println("Расстояние между р1 и р2 " + Point.distance(p1, p2));
+  }
+}
