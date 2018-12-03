abc6645ffc6502350c0a786e526361a8be27ea41
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2018-12-03 22:12:14 +0300

    support tests in IE and FIREFOX browsers are added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,45 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  public ChromeDriver wd;
+  WebDriver wd;
 
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    if (browser==BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    }
+    else if (browser==BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if (browser==BrowserType.IE) {
+      InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+      internetExplorerOptions.setCapability("nativeEvents", false);
+      internetExplorerOptions.setCapability("unexpectedAlertBehaviour", "accept");
+      internetExplorerOptions.setCapability("ignoreProtectedModeSettings", true);
+      internetExplorerOptions.setCapability("disable-popup-blocking", true);
+      internetExplorerOptions.setCapability("enablePersistentHover", true);
+      wd = new InternetExplorerDriver(internetExplorerOptions);
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -1,18 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;;
+import org.openqa.selenium.WebDriver;
 
 public class SessionHelper extends HelperBase {
 
 
-  public SessionHelper(ChromeDriver wd) {
+  public SessionHelper(WebDriver wd) {
     super(wd);
   }
 
   public void login(String user, String pass, By xpath, String username, String password) {
     type(By.name("user"),username);
     type(By.name("pass"),password);
-    click(By.xpath("//input[@value='Login']"));
+    //click(By.xpath("//input[@value='Login']"));
+    click(By.xpath("//*[@id='LoginForm']/input[3]"));
   }
 }
