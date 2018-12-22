f0099a80b21f7b2c9d32bfa219484fab8752204c
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2018-12-23 01:12:33 +0300

    Cache and hash Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -31,9 +31,8 @@ public class GroupDeletionTest extends TestBase {
     Groups before = app.group().all();
     GroupData detetedGroup = before.iterator().next();
     app.group().delete(detetedGroup);
+    assertThat(app.group().count(), equalTo(before.size() -1));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size() - 1);
-
     assertThat(after, equalTo(before.without(detetedGroup)));
   }
 }
