c92ced8f4726d6be0aa502716f24e1858e576e34
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2018-12-25 21:46:35 +0300

    read from file (group example)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.interactions.SourceType;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactCreationTest extends TestBase {
-  @Test(enabled = true)
+public class ContactCreationTest extends TestBase{
+
+
+
+  @Test
   public void testContactCreation() throws Exception {
     app.goTo().HomePage();
     Contacts before = app.contact().all();
