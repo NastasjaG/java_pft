1b959ed35d2e3fc050318fb8916d441601e3a4aa
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2019-01-23 15:41:39 +0300

    New module for Mantis is created

---
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,65 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  private final Properties properties;
+  WebDriver wd;
+
+  private String browser;
+
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+      internetExplorerOptions.setCapability("nativeEvents", false);
+      internetExplorerOptions.setCapability("unexpectedAlertBehaviour", "accept");
+      internetExplorerOptions.setCapability("ignoreProtectedModeSettings", true);
+      internetExplorerOptions.setCapability("disable-popup-blocking", true);
+      internetExplorerOptions.setCapability("enablePersistentHover", true);
+      wd = new InternetExplorerDriver(internetExplorerOptions);
+    }
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+
+  }
+
+  public Properties getProperties() {
+    return properties;
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+  public void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+}
