872b2e0b17a07823274fc8a49a4b93d9ce95cdc4
NastasjaG <45099245+NastasjaG@users.noreply.github.com>
2019-01-27 21:17:55 +0300

    send email on server

--- mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -16,9 +16,12 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser) {
@@ -28,25 +31,7 @@ public class ApplicationManager {
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-
-    if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
-      internetExplorerOptions.setCapability("nativeEvents", false);
-      internetExplorerOptions.setCapability("unexpectedAlertBehaviour", "accept");
-      internetExplorerOptions.setCapability("ignoreProtectedModeSettings", true);
-      internetExplorerOptions.setCapability("disable-popup-blocking", true);
-      internetExplorerOptions.setCapability("enablePersistentHover", true);
-      wd = new InternetExplorerDriver(internetExplorerOptions);
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-
+    properties.load(new FileReader(new File(String.format("src/main/resources/%s.properties", target))));
   }
 
   public Properties getProperties() {
@@ -54,7 +39,9 @@ public class ApplicationManager {
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
 
@@ -62,4 +49,53 @@ public class ApplicationManager {
     wd.findElement(By.linkText("Logout")).click();
   }
 
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public FtpHelper ftp() {
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
+
+  public WebDriver getDriver() {
+    if(wd==null)
+
+      if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+        internetExplorerOptions.setCapability("nativeEvents", false);
+        internetExplorerOptions.setCapability("unexpectedAlertBehaviour", "accept");
+        internetExplorerOptions.setCapability("ignoreProtectedModeSettings", true);
+        internetExplorerOptions.setCapability("disable-popup-blocking", true);
+        internetExplorerOptions.setCapability("enablePersistentHover", true);
+        wd = new InternetExplorerDriver(internetExplorerOptions);
+      }
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+    return wd;
+  }
+
+  public MailHelper mail() {
+    if (mailHelper ==null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+  }
 }
